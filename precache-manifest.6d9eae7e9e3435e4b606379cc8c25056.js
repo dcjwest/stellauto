1ad8175e9e187af20aae480ebe140510c0063e64
David van der Westhuizen <dcjwest@gmail.com>
2020-05-29 13:59:03 +0200

    Updates

---
+++ precache-manifest.6d9eae7e9e3435e4b606379cc8c25056.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d575565b5936fb436e8d5abdf6fd9a5",
+    "url": "/stellauto/index.html"
+  },
+  {
+    "revision": "46b7f4d6865824d042fb",
+    "url": "/stellauto/static/css/main.0fc285c5.chunk.css"
+  },
+  {
+    "revision": "56ccf27c169b3c7b1fc5",
+    "url": "/stellauto/static/js/2.e77b2570.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stellauto/static/js/2.e77b2570.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b7f4d6865824d042fb",
+    "url": "/stellauto/static/js/main.1163d6e1.chunk.js"
+  },
+  {
+    "revision": "de03ae327c2e96392cfb",
+    "url": "/stellauto/static/js/runtime-main.b3038422.js"
+  },
+  {
+    "revision": "b640a2cdf3ba280b0929bac53a789a68",
+    "url": "/stellauto/static/media/car-doc.b640a2cd.jpg"
+  },
+  {
+    "revision": "fa021cd5d43eb1de1338aed3bc35c84b",
+    "url": "/stellauto/static/media/logo.fa021cd5.svg"
+  },
+  {
+    "revision": "243be38fc93e0999b6ed63145761fa75",
+    "url": "/stellauto/static/media/scope-car.243be38f.jpg"
+  }
+]);
