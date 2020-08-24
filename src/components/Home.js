b279e5d14dc847ed5df8803e2aa213e5df17c78e
David van der Westhuizen <dcjwest@gmail.com>
2020-08-24 21:44:41 +0200

    Fixed landing section's layout which broke on mobile with screen rotated to landscape. Also adjusted mobile menu's link spacing and supplier logo link hover styles.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,10 +4,12 @@ import landingImg from '../images/scope-car.jpg';
 const Home = () => {
     return (
         <section id='home' className='container'>
-            <img src={landingImg} alt='Car-shaped stethoscope.' />
             <div className='call-to-action'>
                 <h2>Need emergency car service?<br />We're here to help.</h2>
                 <a className='btn' href='#quote'>Enquire now</a>
+            </div>
+            <div className='img-container'>
+                <img src={landingImg} alt='Car-shaped stethoscope.' />
             </div>
         </section>
     );
